86d7d239464da26f0ab1e1a897e41ada204c0120
manish <gowardipemanish95@gmail.com>
2022-03-08 20:29:32 +0530

    #000: Manish : Try to fix css

--- src/main/scala/org/tmt/sbt/docs/ParadoxMaterialSitePlugin.scala
+++ src/main/scala/org/tmt/sbt/docs/ParadoxMaterialSitePlugin.scala
@@ -18,15 +18,12 @@ object ParadoxMaterialSitePlugin extends AutoPlugin {
   override def projectSettings: Seq[Setting[_]] =
     ParadoxMaterialThemePlugin.paradoxMaterialThemeSettings(Compile) ++
       Seq(
-        Compile / paradoxMaterialTheme := {
-          ParadoxMaterialTheme()
-            .withCustomStylesheet("assets/stylesheets/borer.css")
-        },
         Compile / paradox / sourceDirectory                := baseDirectory.value / "src" / "main",
         Compile / paradox / paradoxTheme / sourceDirectory := (Compile / paradox / sourceDirectory).value / "_template",
         Compile / paradox / paradoxMaterialTheme           := {
           ParadoxMaterialTheme()
             .withFavicon("assets/tmt_favicon.ico")
+            .withCustomStylesheet("assets/stylesheets/docs.css")
             .withRepository(new URI(gitCurrentRepo.value))
         },
         Compile / paradoxProperties ++= Map(
